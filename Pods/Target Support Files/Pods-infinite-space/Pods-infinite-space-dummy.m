79ef422ec652149d4b067ac48ee40ae7b2e246bf
Komlan Attiogbe <jonathan.attiogbe@gmail.com>
2019-04-08 21:49:37 -0400

    testing deletion

--- Pods/Target Support Files/Pods-infinite-space/Pods-infinite-space-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_infinite_space : NSObject
-@end
-@implementation PodsDummy_Pods_infinite_space
-@end
--- Pods/Target Support Files/Pods-infinite-spaceUITests/Pods-infinite-spaceUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_infinite_spaceUITests : NSObject
-@end
-@implementation PodsDummy_Pods_infinite_spaceUITests
-@end
