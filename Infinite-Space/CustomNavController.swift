05cca54343a33f41668973d55e1207d55df669ad
Komlan Attiogbe <jonathan.attiogbe@gmail.com>
2019-04-22 16:03:12 -0400

    Updating navigation controllers

---
+++ Infinite-Space/CustomNavController.swift
@@ -0,0 +1,13 @@
+//
+//  CustomNavController.swift
+//  Infinite-Space
+//
+//  Created by Komlan Attiogbe on 4/22/19.
+//  Copyright © 2019 aincrad-online. All rights reserved.
+//
+
+import UIKit
+
+class CustomNavController: UINavigationController {
+
+}
