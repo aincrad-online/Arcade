3fb43d857d477a1e0a816eccc2cd6d429e177b4c
Caleb Giles <calebgiles@calebs-macbook-pro.local>
2019-04-22 13:34:39 -0400

    Player ship can be moved

--- Infinite-Space/GameScene.swift
+++ Infinite-Space/GameScene.swift
@@ -11,17 +11,41 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    
+    var player = SKSpriteNode()
     
     
     override func didMove(to view: SKView) {
         
-
+        player = self.childNode(withName: "Player") as! SKSpriteNode
  
         
-}
+    }
     
-
+    func fireBullet(){
+        
+        //let bullet = SKSpriteNode
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        for touch in touches{
+            let location = touch.location(in: self)
+            
+            player.run(SKAction.moveTo(x: location.x, duration: 0.2))
+            
+        }
+        
+        
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches{
+            let location = touch.location(in: self)
+            
+            player.run(SKAction.moveTo(x: location.x, duration: 0.1))
+            
+        }
+    }
     
     
     override func update(_ currentTime: TimeInterval) {
